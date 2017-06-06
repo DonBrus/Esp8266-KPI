3baed5009c05690f7eb4060cf3cd5d580e667262
Donato Brusamento <fancydadu@gmail.com>
2017-06-06 08:51:59 +0200

    ..

--- KPI.ino
+++ KPI.ino
@@ -4,6 +4,9 @@
 
 KP kpi("Huzzah");
 
+#define SLAVE_INT_PIN 12
+#define SLAVE_INT_IN 14
+
 #define NETSSID "Alice-55361560"
 #define NETPSW  "20c1af385935b78a3mea1385"
 #define IP 10010, 192, 168, 1, 14
@@ -12,12 +15,17 @@ KP kpi("Huzzah");
 //#define NETPSW  "abcd1234"
 //#define IP 10010, 192, 168, 43, 52
 
+boolean armOk = true, reported = false;
+
 void setup() {
   Serial.begin(115200);
   Wire.begin();   //join i2c as master
 
-  pinMode(12, OUTPUT);  //used for master triggered interrupt
-  digitalWrite(12, HIGH);
+  pinMode(SLAVE_INT_PIN, OUTPUT);  //used for master triggered interrupt
+  digitalWrite(SLAVE_INT_PIN, HIGH);
+
+  pinMode(SLAVE_INT_IN, INPUT);  //used for master triggered interrupt
+  attachInterrupt(SLAVE_INT_IN, Report, FALLING);
 
   do {
     kpi.begin(NETSSID, NETPSW, IP);
@@ -69,27 +77,88 @@ void setup() {
   //
 
 
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println("Subbed. Going to Loop...");
 }
 
 void loop() {
 
-  if (kpi.notification()) {
+  static boolean newTask = false;
+  static char Task = 's'; //byte to send to the slave whenever there's a change in context
+
+  if (kpi.notification() && armOk) {
+
     Serial.println("NEW RESULTS!");
     Result *r = kpi.getResults();
     checkResults(r);
 
-    if (strstr(r->var1, "#Stop") != NULL) Serial.println("ARM : STOP");
-    else  if (strstr(r->var1, "#Task1") != NULL)  Serial.println("ARM : TASK 1");
-    else  if (strstr(r->var1, "#Task2") != NULL)  Serial.println("ARM : TASK 2");
-    else Serial.println("ARM : TASK NOT FOUND");
+    newTask = true;
+
+    if (strstr(r->var1, "#Stop") != NULL) {
+      Serial.println("ARM : STOP");
+      Task = 's';
+    }
+
+    else  if (strstr(r->var1, "#Task1") != NULL) {
+      Serial.println("ARM : TASK 1");
+      Task = '1';
+    }
+
+    else  if (strstr(r->var1, "#Task2") != NULL) {
+      Serial.println("ARM : TASK 2");
+      Task = '2';
+    }
+
+    else {
+      Serial.println("ARM : TASK NOT FOUND");
+      Task = 'n'; //null , for error management
+      newTask = false; //don't wake slave up
+    }
+
+    if (newTask) {
+
+      Serial.print("Writing to Slave: ");
+      Serial.println(Task);
+
+      digitalWrite(SLAVE_INT_PIN, LOW); //inform slave of new task
+      delay(100); //wait a bit so we're sure it's been received
+      digitalWrite(SLAVE_INT_PIN, HIGH); //release
+
+      Wire.beginTransmission(8); // transmit to device #8
+
+      Wire.write(Task); //send new Task
+
+      Wire.endTransmission();
+
+      newTask = false;
+    }
 
   }
-  delay(1000);
+
+  else if (!armOk && !reported) {
+    Serial.println("REPORTING TO SUB...");
+
+    reported = true;
+
+    Triple  *list = new Triple;
+
+    strcpy(list->subject, "http://arduArm.com/ss#MeArm");
+    strcpy(list->predicate, "http://arduArm.com/ss#HasStatus");
+    strcpy(list->object, "http://arduArm.com/ss#Faulty");
+    list->next = NULL;
+    checkList(list);
+
+    if (kpi.insert(list)) Serial.println("REPORTED");
+
+    yield();
+
+
+    delay(1000);
+
+  }
 
 }
 
-void checkList(Triple *list) {
+void checkList(Triple * list) {
   Triple *index = list;
   byte i = 1;
   while (index != NULL) {
@@ -103,7 +172,7 @@ void checkList(Triple *list) {
   }
 }
 
-void checkResults(Result *list) {
+void checkResults(Result * list) {
   Result *index = list;
   byte i = 1;
   while (index != NULL) {
@@ -115,5 +184,24 @@ void checkResults(Result *list) {
     Serial.println(index->var3);
     index = index->next;
   }
+}
+
+void Report() {
+
+  char c;
+
+  Serial.println("SLAVE REPORTING");
+
+  Wire.requestFrom(8, 1);    // request 1 byte (sensor data or error)
+
+  while (Wire.available()) { // slave may send less than requested
+    c = Wire.read(); // receive a byte as character
+    Serial.print(c);         // print the character
+  }
+
+  if (c == 'F' or c == 'f') {
+    armOk = false;
+  }
+
 }
 
